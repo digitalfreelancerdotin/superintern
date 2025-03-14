f8f36c2cd9a158c54e62a7a5b98743a2be2b2837
digitalfreelancerdotin <75946237+digitalfreelancerdotin@users.noreply.github.com>
2025-03-14 16:02:22 +0530

    added reset option

---
+++ restart.dev.bat
@@ -0,0 +1,13 @@
+@echo off
+echo Stopping any running Node processes...
+taskkill /F /IM node.exe
+
+echo Cleaning up files...
+rmdir /s /q node_modules
+del package-lock.json
+
+echo Installing dependencies...
+npm install
+
+echo Starting dev server...
+npm run dev
